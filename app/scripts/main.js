192a97c7d2ed1f757788d5e108b91a24a2cefd09
KMcWeeney22 <mcweeney@seattleu.edu>
2016-10-07 23:55:39 -0700

    finished adding alternate theme

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,5 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+document.getElementById('change-theme').onclick = function () {
+    document.body.classList.toggle('alternate-theme');
+};
